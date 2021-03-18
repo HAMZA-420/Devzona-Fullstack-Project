7e20896626b457df8603b0c5c5139e645091d7e2
MUHAMMAD HAMZA KHAN <mhamzakhan.cui@gmail.com>
2021-03-18 18:11:13 +0500

    Adding header features-Menu

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -7,6 +7,8 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Button from '@material-ui/core/Button';
 import {Link} from "react-router-dom";
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
 function ElevationScroll(props) {
     const { children } = props;
@@ -50,6 +52,18 @@ const useStyles= makeStyles(theme => ({
         marginLeft: "50px",
         marginRight: "25px",
         height: "45px",
+    },
+    menu:{
+        backgroundColor: theme.palette.common.blue,
+        color: "white",
+        borderRadius: "0px"
+    },
+    menuItem: {
+        ...theme.typography.tab,
+        opacity: 0.7,
+        "&:hover" : {
+            opacity: 1
+        }
     }
 
 }))
@@ -57,28 +71,110 @@ const useStyles= makeStyles(theme => ({
 export default function Header(props) {
     const classes = useStyles();
     const [value, setValue] = useState(0);
+    const [anchorEl, setAnchorEl] = useState(null);
+    const [open, setOpen] = useState(false);
+    const [selectedIndex, setSelectedIndex] = useState(0);
+
+
     const handleChange = (e, value) => {
         setValue(value);
     }
+
+    const handleClick = (e) => {
+        setAnchorEl(e.currentTarget)
+        setOpen(true)
+    }
+
+    const handleMenuItemClick = (e, i) => {
+        setAnchorEl(null);
+        setOpen(false);
+        setSelectedIndex(i)
+    }
+
+    const handleClose = (e) => {
+        setAnchorEl(null)
+        setOpen(false)
+    }
+
+    const menuOptions = [{name: "Services" , link: "/services"}, {name: "Custom Software Development", link: "/customsoftware"}, {name: "Mobile App Development", link: "/mobileapps"}, {name: "Website Development", link: "/websites"}]
 
     useEffect(() => {
         if(window.location.pathname ==="/" && value !==0) {
             setValue(0)
         }
-        else if(window.location.pathname == "/services" && value!==1) {
+        else if(window.location.pathname === "/services" && value!==1) {
             setValue(1)
         }
-        else if(window.location.pathname == "/revolution" && value!==2) {
+        else if(window.location.pathname === "/revolution" && value!==2) {
             setValue(2)
         }
-        else if(window.location.pathname == "/about" && value!==3) {
+        else if(window.location.pathname === "/about" && value!==3) {
             setValue(3)
         }
-        else if(window.location.pathname == "/contact" && value!==4) {
+        else if(window.location.pathname === "/contact" && value!==4) {
             setValue(4)
         }
-        else if(window.location.pathname == "/estimate" && value!==5) {
+        else if(window.location.pathname === "/estimate" && value!==5) {
             setValue(5)
+        }
+
+        switch(window.location.pathname) {
+            case "/":
+                if(value !==0) {
+                    setValue(0)
+                }
+                break;
+            case "/services":
+                if(value !==1) {
+                    setValue(1)
+                    setSelectedIndex(0)
+                 }
+                 break;
+            case "/customsoftware":
+                 if(value !==1) {
+                  setValue(1)
+                  setSelectedIndex(1)
+                }
+                break;
+            case "/mobileapps":
+                if(value !==1) {
+                    setValue(1);
+                    setSelectedIndex(2)
+                }
+                break;
+            case "/websites":
+                if(value !==1) {
+                    setValue(1);
+                    setSelectedIndex(3)
+                }
+                break;
+            case "/revolution": 
+                if(value !==2) {
+                    setValue(2)
+                }
+                break;  
+            
+            case "/about": 
+                if(value !==2) {
+                    setValue(3)
+                }
+                break;  
+            
+            case "/contact": 
+                if(value !==4) {
+                    setValue(4)
+                }
+                break;  
+            
+            case "/estimate": 
+                if(value !==5) {
+                    setValue(5)
+                } 
+                break; 
+            
+
+            default:
+                break;
         }
 
     }, [value]);
@@ -98,7 +194,7 @@ export default function Header(props) {
             indicatorColor="primary"
             >
                 <Tab className={classes.tab} label="Home" component={Link} to="/" />
-                <Tab className={classes.tab} label="Services" component={Link} to="/services"/>
+                <Tab aria-owns={anchorEl ? "simple-menu" : undefined} aria-haspopup={anchorEl ? "true" : undefined} onMouseOver={event => handleClick(event)} className={classes.tab} label="Services" component={Link} to="/services"/>
                 <Tab className={classes.tab} label="The Revolution" component={Link} to="/revolution"/>
                 <Tab className={classes.tab} label="About Us" component={Link} to="/about"/>
                 <Tab className={classes.tab} label="Contact Us" component={Link} to="/contact"/>
@@ -106,6 +202,17 @@ export default function Header(props) {
             <Button variant="contained" color="secondary" className={classes.button}>
                 Free Estimate
             </Button>
+            <Menu id="simple-menu" anchorEl={anchorEl} open={open} onClose={handleClose} MenuListProps={{onMouseLeave: handleClose}} classes={{paper: classes.menu}} elevation={0}>
+             {menuOptions.map((option, i) => (
+                 <MenuItem key={option} component={Link} to={option.link} 
+                 classes={{root: classes.menuItem}}
+                 onClick={(event) => {handleMenuItemClick(event,i); setValue(1); handleClose()}}
+                 selected={i=== selectedIndex && value ===1}
+                 >
+                   {option.name}
+                 </MenuItem>
+             ))}
+           </Menu>
             </Toolbar>
            
         </AppBar>
