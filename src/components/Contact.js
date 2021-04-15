2e485536567bb8645ab9c1730bc14c2c8c9362e7
MUHAMMAD HAMZA KHAN <mhamzakhan.cui@gmail.com>
2021-04-15 11:43:35 +0500

    cloud function, snackbars, network request

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,5 @@
 import React,{useState} from 'react';
+import axios from 'axios';
 import {makeStyles, useTheme} from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
@@ -14,6 +15,8 @@ import {Link} from 'react-router-dom';
 import mobileBackground from '../assets/mobileBackground.jpg'
 import Dialog from '@material-ui/core/Dialog';
 import DialogContent from '@material-ui/core/DialogContent';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Snackbar from '@material-ui/core/Snackbar';
 
 
 const useStyles = makeStyles(theme => ({
@@ -74,7 +77,9 @@ const useStyles = makeStyles(theme => ({
         height: 40,
         width: 225
     }
-}))
+}));
+
+
 
 export default function Contact(props) {
 
@@ -91,6 +96,9 @@ export default function Contact(props) {
     const [emailHelper, setEmailHelper] = useState("");
     const [phoneHelper, setPhoneHelper] = useState("");
     const [open, setOpen] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [alert, setAlert] = useState({open: false, message: "",
+    backgroundColor: ""})
 
     const onChange= event => {
         let valid;
@@ -123,7 +131,40 @@ export default function Contact(props) {
                  break;
                     
         }
-    }
+    };
+
+    const onConfirm=() => {
+        setLoading(true)
+        axios.get(
+            " ",{params: {
+                name: name,
+                email: email,
+                phone: phone,
+                message: message
+            }}   
+        )
+        .then(res =>{
+            setLoading(false)
+            setOpen(false)
+            setName("")
+            setEmail("")
+            setPhone("")
+            setMessage("")
+            setAlert({open: true, message: "Message sent successfully!", 
+            backgroundColor: "#4BB543"     
+        })
+        })
+        .catch(err =>{setLoading(false); setAlert({open: true, message:
+        "Something went wrong, please try again", backgroundColor: "#FF3232"})});
+    };
+
+    const buttonContents = (
+        <React.Fragment>
+            Send message
+            <img src={airplane} style={{marginLeft: "1em"}} alt="paper airplane" />                  
+        </React.Fragment>
+    )
+
     return(
         <Grid container direction="row">
             <Grid 
@@ -219,9 +260,8 @@ export default function Contact(props) {
                     className={classes.sendButton}
                     onClick={() => setOpen(true)}
                     >
-                        Send message
-                    <img src={airplane} style={{marginLeft: "1em"}} alt="paper airplane" />
-                    </Button>
+                      {buttonContents}
+                      </Button>
                 </Grid>
             </Grid>
             </Grid>
@@ -306,15 +346,20 @@ export default function Contact(props) {
                               phoneHelper.length!==0 || emailHelper.length !==0 || message.length===0}
                               variant="contained" 
                               className={classes.sendButton}
-                              onClick={() => setOpen(true)}
+                              onClick={onConfirm}
                               >
-                        Send message
-                         <img src={airplane} style={{marginLeft: "1em"}} alt="paper airplane" />
+                         {loading ? <CircularProgress size={30} />: buttonContents}
                          </Button>
                         </Grid>
                     </Grid>
                 </DialogContent>
             </Dialog>
+            <Snackbar open={alert.open}
+             message={alert.message} 
+             ContentProps={{style: {backgroundColor: alert.backgroundColor}}} 
+             anchorOrigin={{vertical: "top", horizontal: "center"}} onClose={() => setAlert({...alert, open: false})}
+             autoHideDuration={4000}
+             />
             <Grid 
             item 
             container
